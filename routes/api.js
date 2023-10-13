72bc9deb510913eb74bc7fb17c36b97b377a4180
joel <joelthobias447@gmail.com>
2023-10-13 23:09:33 +0530

    week 4 post using api

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,30 @@
 import express from 'express';
-const apiRouter = express.Router();
+import db from '../connection.js';
 
+const apiRouter = express.Router();
 apiRouter.get('/',(req,res)=>{
     res.json({
         msg:"hello",
         user:"JOEL"
     })
+})
+
+apiRouter.post('/user/login',async(req,res)=>{
+    const userData = req.body;
+    const userCollection = db.get().collection('user');
+    const user = await userCollection.findOne({ 'email': userData.email },{'password':userData.password});
+    if (user) {
+        res.status(200).cookie('user', user).json({
+            status:'Sucess',
+            user:req.body
+        });
+    } else {
+        let msg = "Invalid Credentials";
+        res.status(400).json({
+            status:'failed',
+            msg
+        })
+    }
 })
 
 export default apiRouter;
